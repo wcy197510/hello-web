b8f68c3b7f1ddc301083cdbd9febb8476bfc6794
wcy197510 <3476827965@qq.com>
2019-12-05 20:46:05 +0800

    MySQL(2)select,where,order by,update,delete子句

--- demo-php/test07.php
+++ demo-php/test07.php
@@ -23,14 +23,17 @@ if ($conn->connect_error) {
 echo "连接成功".'<br>';
 
 //创建数据库
+/*
 $sql = "CREATE DATABASE myDB";
 if ($conn->query($sql) == true) {
     echo "数据库创建成功".'<br>';
 } else {
     echo "Error creating database:".$conn->error.'<br>';
 }
+*/
 
 //创建数据表
+/*
 $sql = "CREATE TABLE MyGuests(
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
 firstname VARCHAR(30) NOT NULL ,
@@ -44,8 +47,10 @@ if ($conn->query($sql) == true) {
 } else {
     echo "创建数据表错误：".$conn->error.'<br>';
 }
+*/
 
 //插入记录
+
 //插入多条记录时，每个sql语句必须用分号隔开
 /*
 $sql="INSERT INTO MyGuests (firstname,lastname,email)
@@ -62,7 +67,8 @@ if ($conn->multi_query($sql)==TRUE)
     echo "Error:".$sql.'<br>'.$conn->error;
 }
 */
-//使用预处理语句
+//使用预处理语句插入记录
+/*
 //预处理及绑定
 $stmt = $conn->prepare("INSERT INTO MyGuests(firstname,lastname,email)
 VALUES (?,?,?)");
@@ -82,6 +88,55 @@ $stmt->execute();
 echo "新纪录插入成功！";
 
 $stmt->close();
+*/
+
+//读取数据
+/*
+$sql="SELECT id,firstname,lastname from MyGuests";
+$result=$conn->query($sql);
+
+if($result->num_rows>0)
+{
+    //输出数据
+    //fetch_assoc()将结果集放入关联数组并循环输出
+    while ($row=$result->fetch_assoc()){
+        echo "id:".$row["id"]."-name:".$row["firstname"]." ".$row["lastname"].'<br>';
+    }
+}else{
+    echo "0 结果";
+}
+*/
+
+//where子句
+/*
+$result=mysqli_query($conn,"SELECT * FROM MyGuests
+WHERE firstname='John'");
+
+while ($row=mysqli_fetch_array($result))
+{
+    echo $row['firstname']." ".$row["lastname"].'<br>';
+}
+*/
+
+//order by子句
+/*
+$result=mysqli_query($conn,"SELECT * FROM MyGuests
+ORDER BY id");
+
+while ($row=mysqli_fetch_array($result))
+{
+    echo $row["id"]." ".$row['firstname']." ".$row["lastname"].'<br>';
+}
+*/
+
+//update子句
+mysqli_query($conn, "UPDATE MyGuests set lastname='MMo'
+where firstname='Mary'");
+
+//删除数据库数据
+mysqli_query($conn, "DELETE FROM MyGuests
+WHERE lastname='MMo'");
+
 $conn->close();
 ?>
 </body>
